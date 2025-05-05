60e24329da7bad1b635a60de440c07e9c9dbed5c
Ioan-Horia Rotaru <me@horiarotaru.com>
2025-05-05 17:27:17 +0100

    Updated eslint in none-ls. Updated alpha config to be more simple

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,12 +1,40 @@
 return {
-	"goolord/alpha-nvim",
-	-- dependencies = { 'echasnovski/mini.icons' },
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		local startify = require("alpha.themes.startify")
-		-- available: devicons, mini, default is mini
-		-- if provider not loaded and enabled is true, it will try to use another provider
-		startify.file_icons.provider = "devicons"
-		require("alpha").setup(startify.config)
-	end,
+  "goolord/alpha-nvim",
+  event = "VimEnter",
+  enabled = true,
+  init = false,
+  opts = function()
+    local dashboard = require("alpha.themes.dashboard")
+    local logo = [[
+__/\\\\\_____/\\\_______________________________/\\\________/\\\___________________________        
+ _\/\\\\\\___\/\\\______________________________\/\\\_______\/\\\___________________________       
+  _\/\\\/\\\__\/\\\______________________________\//\\\______/\\\___/\\\_____________________      
+   _\/\\\//\\\_\/\\\_____/\\\\\\\\______/\\\\\_____\//\\\____/\\\___\///_____/\\\\\__/\\\\\___     
+    _\/\\\\//\\\\/\\\___/\\\/////\\\___/\\\///\\\____\//\\\__/\\\_____/\\\__/\\\///\\\\\///\\\_    
+     _\/\\\_\//\\\/\\\__/\\\\\\\\\\\___/\\\__\//\\\____\//\\\/\\\_____\/\\\_\/\\\_\//\\\__\/\\\_   
+      _\/\\\__\//\\\\\\_\//\\///////___\//\\\__/\\\______\//\\\\\______\/\\\_\/\\\__\/\\\__\/\\\_  
+       _\/\\\___\//\\\\\__\//\\\\\\\\\\__\///\\\\\/________\//\\\_______\/\\\_\/\\\__\/\\\__\/\\\_ 
+        _\///_____\/////____\//////////_____\/////___________\///________\///__\///___\///___\///__
+    ]]
+
+    dashboard.section.header.val = vim.split(logo, "\n")
+    -- stylua: ignore
+    dashboard.section.buttons.val = {
+      dashboard.button("f", " " .. " Find file", "<cmd> Telescope find_files <cr>"),
+      dashboard.button("l", "󰒲 " .. " Lazy", "<cmd> Lazy <cr>"),
+      dashboard.button("q", " " .. " Quit", "<cmd> qa <cr>"),
+    }
+    for _, button in ipairs(dashboard.section.buttons.val) do
+      button.opts.hl = "AlphaButtons"
+      button.opts.hl_shortcut = "AlphaShortcut"
+    end
+    dashboard.section.header.opts.hl = "AlphaHeader"
+    dashboard.section.buttons.opts.hl = "AlphaButtons"
+    dashboard.section.footer.opts.hl = "AlphaFooter"
+    dashboard.opts.layout[1].val = 8
+    return dashboard
+  end,
+  config = function(_, dashboard)
+    require("alpha").setup(dashboard.opts)
+  end,
 }
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,3 +1,29 @@
+local function has_eslint_config_upwards(start_path)
+	local config_files = {
+		".eslintrc.js",
+		".eslintrc.cjs",
+		".eslintrc.json",
+		".eslintrc.yaml",
+		".eslintrc.yml",
+		"eslint.config.js",
+		"eslint.config.mjs",
+	}
+
+	local Path = require("plenary.path")
+	local path = Path:new(start_path)
+
+	while path.filename ~= "/" do
+		for _, config in ipairs(config_files) do
+			if Path:new(path, config):exists() then
+				return true
+			end
+		end
+		path = path:parent()
+	end
+
+	return false
+end
+
 return {
 	"nvimtools/none-ls.nvim",
 	dependencies = {
@@ -13,7 +39,13 @@ return {
 					filetypes = { "javascript", "typescript", "astro", "json", "css", "html" },
 				}),
 				null_ls.builtins.completion.spell,
-				require("none-ls.diagnostics.eslint_d"),
+				require("none-ls.diagnostics.eslint_d").with({
+					condition = function()
+						local buf_path = vim.api.nvim_buf_get_name(0)
+						local start_dir = vim.fn.fnamemodify(buf_path, ":p:h")
+						return has_eslint_config_upwards(start_dir)
+					end,
+				}),
 				null_ls.builtins.diagnostics.rubocop,
 				null_ls.builtins.formatting.rubocop,
 			},
