4eed111da22afb58822d09e49f70d464339b8c5f
Ioan-Horia Rotaru <me@horiarotaru.com>
2025-05-13 23:37:22 +0100

    Format alpha.lua file

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,11 +1,11 @@
 return {
-  "goolord/alpha-nvim",
-  event = "VimEnter",
-  enabled = true,
-  init = false,
-  opts = function()
-    local dashboard = require("alpha.themes.dashboard")
-    local logo = [[
+	"goolord/alpha-nvim",
+	event = "VimEnter",
+	enabled = true,
+	init = false,
+	opts = function()
+		local dashboard = require("alpha.themes.dashboard")
+		local logo = [[
   ⠀⠀⠀⠀⠀⠀⠀⣤⣤⣤⣄⣀   ⠀⠀⠀⣀⣀⣀⣀⠀⠀⠀⠀⠀
   ⠀⠀⠀⠀⠀⠀⢰⡏⢻⣫⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⠟⣿⠀⠀⠀⠀⠀
   ⠀⠀⠀⠀⡐⡄⣸⣰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣄⣿⠀⠀⠀⠀⠀
@@ -19,7 +19,7 @@ return {
 ░▒█░░▀█░▀▀▀░░▀▀░░░░▀▄▀░░▀▀▀░▀░░▒▀
     ]]
 
-    dashboard.section.header.val = vim.split(logo, "\n")
+		dashboard.section.header.val = vim.split(logo, "\n")
     -- stylua: ignore
     dashboard.section.buttons.val = {
       dashboard.button("f", " " .. " Find file", "<cmd> Telescope find_files <cr>"),
@@ -27,17 +27,17 @@ return {
       dashboard.button("s", "" .. " Sessions", "<cmd> Telescope persisted<cr>"),
       dashboard.button("q", " " .. " Quit", "<cmd> qa <cr>"),
     }
-    for _, button in ipairs(dashboard.section.buttons.val) do
-      button.opts.hl = "AlphaButtons"
-      button.opts.hl_shortcut = "AlphaShortcut"
-    end
-    dashboard.section.header.opts.hl = "AlphaHeader"
-    dashboard.section.buttons.opts.hl = "AlphaButtons"
-    dashboard.section.footer.opts.hl = "AlphaFooter"
-    dashboard.opts.layout[1].val = 8
-    return dashboard
-  end,
-  config = function(_, dashboard)
-    require("alpha").setup(dashboard.opts)
-  end,
+		for _, button in ipairs(dashboard.section.buttons.val) do
+			button.opts.hl = "AlphaButtons"
+			button.opts.hl_shortcut = "AlphaShortcut"
+		end
+		dashboard.section.header.opts.hl = "AlphaHeader"
+		dashboard.section.buttons.opts.hl = "AlphaButtons"
+		dashboard.section.footer.opts.hl = "AlphaFooter"
+		dashboard.opts.layout[1].val = 8
+		return dashboard
+	end,
+	config = function(_, dashboard)
+		require("alpha").setup(dashboard.opts)
+	end,
 }
