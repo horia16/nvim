21ffba47272f1f3cf03aad69786dff18278b7df6
Horia Rotaru <horia@cronofy.com>
2025-05-14 16:22:08 +0100

    Fix indentation. Update some keymaps. Add comment.lua

--- lua/config/vim.lua
+++ lua/config/vim.lua
@@ -11,13 +11,13 @@ vim.cmd("set number")
 -- SHORTCUTS
 
 vim.keymap.set("n", "<leader>i", function()
-	vim.lsp.buf.format({ async = true })
+  vim.lsp.buf.format({ async = true })
 end, { desc = "Format entire file via LSP", silent = true })
 
 vim.keymap.set("n", "<leader>I", function()
-	local save_cursor = vim.api.nvim_win_get_cursor(0)
-	vim.cmd("normal! gg=G")
-	vim.api.nvim_win_set_cursor(0, save_cursor)
+  local save_cursor = vim.api.nvim_win_get_cursor(0)
+  vim.cmd("normal! gg=G")
+  vim.api.nvim_win_set_cursor(0, save_cursor)
 end, { desc = "Indent via Vim", silent = true })
 
 vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
@@ -28,47 +28,83 @@ vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
 vim.keymap.set("n", "<C-\\>", "<C-w>p", { desc = "Switch to previous window" })
 
 vim.keymap.set("n", "<leader>v", ":vsplit<CR>", {
-	noremap = true,
-	silent = true,
-	desc = "Vertical Split",
+  noremap = true,
+  silent = true,
+  desc = "Vertical Split",
 })
 
 vim.keymap.set("n", "<leader>.", ":vertical resize +5<CR>", {
-	noremap = true,
-	silent = true,
-	desc = "Increase Width",
+  noremap = true,
+  silent = true,
+  desc = "Increase Width",
 })
 
 vim.keymap.set("n", "<leader>,", ":vertical resize -5<CR>", {
-	noremap = true,
-	silent = true,
-	desc = "Decrease Width",
+  noremap = true,
+  silent = true,
+  desc = "Decrease Width",
 })
 
 vim.api.nvim_create_user_command("SmartQuit", function()
-	-- Close Neo-tree filesystem
-	vim.cmd("Neotree filesystem close")
-	-- Save session
-	vim.cmd("SessionSave")
-	-- Write all and quit
-	vim.cmd("wqa")
+  -- Close Neo-tree filesystem
+  vim.cmd("Neotree filesystem close")
+  -- Save session
+  vim.cmd("SessionSave")
+  -- Write all and quit
+  vim.cmd("wqa")
 end, {})
 
 -- Smart case-sensitive searching
 vim.o.ignorecase = true -- Ignore case by default
-vim.o.smartcase = true -- But make it case-sensitive if the search contains uppercase
+vim.o.smartcase = true  -- But make it case-sensitive if the search contains uppercase
 
 -- Clear search highlights when pressing <Esc>
 vim.keymap.set("n", "<Esc>", function()
-	if vim.v.hlsearch == 1 then
-		vim.cmd("nohlsearch")
-		vim.fn.setreg("/", "")
-	else
-		-- send actual <Esc> key
-		return "<Esc>"
-	end
+  if vim.v.hlsearch == 1 then
+    vim.cmd("nohlsearch")
+    vim.fn.setreg("/", "")
+  else
+    -- send actual <Esc> key
+    return "<Esc>"
+  end
 end, { expr = true, noremap = true, silent = true, desc = "Clear search highlight or escape" })
 
 -- Visual mode indentation with Tab and Shift-Tab
 vim.keymap.set("v", "<Tab>", ">gv", { desc = "Indent right and reselect" })
 vim.keymap.set("v", "<S-Tab>", "<gv", { desc = "Indent left and reselect" })
+
+vim.api.nvim_create_user_command("CopyPath", function(context)
+  local full_path = vim.fn.glob("%:p")
+
+  local file_path = nil
+  if context["args"] == "nameonly" then
+    file_path = vim.fn.fnamemodify(full_path, ":t")
+  end
+
+  -- get the file path relative to project root
+  if context["args"] == "relative" then
+    local project_marker = { ".git", "pyproject.toml" }
+    local project_root = vim.fs.root(0, project_marker)
+    if project_root == nil then
+      vim.print("can not find project root")
+      return
+    end
+
+    file_path = string.gsub(full_path, project_root, "<project-root>")
+  end
+
+  if context["args"] == "absolute" then
+    file_path = full_path
+  end
+
+  vim.fn.setreg("+", file_path)
+  vim.print("Filepath copied to clipboard!")
+end, {
+  bang = false,
+  nargs = 1,
+  force = true,
+  desc = "Copy current file path to clipboard",
+  complete = function()
+    return { "nameonly", "relative", "absolute" }
+  end,
+})
--- lua/plugins/barbar.lua
+++ lua/plugins/barbar.lua
@@ -12,23 +12,40 @@ return {
       animation = true,
       auto_hide = false,
       tabpages = true,
-      clickable = true,
+      clickable = false,
       icons = {
-        buffer_index= true,
+        buffer_index = true,
+        inactive = { button = '' },
       }
     })
     local map = vim.keymap.set
     local opts = { noremap = true, silent = true }
 
-    -- Leader + number (1-9) to go to buffer position
+    -- Leader + number (1-9): Jump to buffer in that position
     for i = 1, 9 do
-      map("n", "<leader>" .. i, "<Cmd>BufferGoto " .. i .. "<CR>", opts)
+      map("n", "<leader>b" .. i, "<Cmd>BufferGoto " .. i .. "<CR>", vim.tbl_extend("force", opts, {
+        desc = "Go to buffer " .. i
+      }))
     end
-    -- Leader + 0 to go to last buffer
-    map("n", "<leader>0", "<Cmd>BufferLast<CR>", opts)
 
-    -- Leader + n / N for next / previous buffer
-    map("n", "<leader>n", "<Cmd>BufferNext<CR>", opts)
-    map("n", "<leader>N", "<Cmd>BufferPrevious<CR>", opts)
+    -- Leader + 0: Go to the last buffer
+    map("n", "<leader>b0", "<Cmd>BufferLast<CR>", vim.tbl_extend("force", opts, {
+      desc = "Go to last buffer"
+    }))
+
+    -- Leader + n: Go to the next buffer
+    map("n", "<leader>bn", "<Cmd>BufferNext<CR>", vim.tbl_extend("force", opts, {
+      desc = "Next buffer"
+    }))
+
+    -- Leader + N: Go to the previous buffer
+    map("n", "<leader>bN", "<Cmd>BufferPrevious<CR>", vim.tbl_extend("force", opts, {
+      desc = "Previous buffer"
+    }))
+
+    -- Leader + c: Close current buffer
+    map("n", "<leader>bc", "<Cmd>BufferClose<CR>", vim.tbl_extend("force", opts, {
+      desc = "Close buffer"
+    }))
   end
 }
