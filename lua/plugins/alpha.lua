7f8bdde4d821a279f06788c9c5675ff351f7c087
Ioan-Horia Rotaru <me@horiarotaru.com>
2025-05-14 23:32:13 +0100

    Dynamic logo color.

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,11 +1,11 @@
 return {
-	"goolord/alpha-nvim",
-	event = "VimEnter",
-	enabled = true,
-	init = false,
-	opts = function()
-		local dashboard = require("alpha.themes.dashboard")
-		local logo = [[
+  "goolord/alpha-nvim",
+  event = "VimEnter",
+  enabled = true,
+  init = false,
+  opts = function()
+    local dashboard = require("alpha.themes.dashboard")
+    local logo = [[
   ⠀⠀⠀⠀⠀⠀⠀⣤⣤⣤⣄⣀   ⠀⠀⠀⣀⣀⣀⣀⠀⠀⠀⠀⠀
   ⠀⠀⠀⠀⠀⠀⢰⡏⢻⣫⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⠟⣿⠀⠀⠀⠀⠀
   ⠀⠀⠀⠀⡐⡄⣸⣰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣄⣿⠀⠀⠀⠀⠀
@@ -19,27 +19,43 @@ return {
 ░▒█░░▀█░▀▀▀░░▀▀░░░░▀▄▀░░▀▀▀░▀░░▒▀
     ]]
 
-		dashboard.section.header.val = vim.split(logo, "\n")
-    -- stylua: ignore
+    local colors = {
+      "#ff5555", -- red
+      "#f1fa8c", -- yellow
+      "#50fa7b", -- green
+      "#8be9fd", -- cyan
+      "#bd93f9", -- purple
+      "#ff79c6", -- pink
+      "#9aedfe", -- blue
+    }
+
+    local random_color = colors[math.random(#colors)]
+    vim.api.nvim_set_hl(0, "AlphaLogo", { fg = random_color })
+
+    dashboard.section.header.val = vim.split(logo, "\n")
+    dashboard.section.header.opts.hl = "AlphaLogo"
+
     dashboard.section.buttons.val = {
-      dashboard.button("f", " " .. " Find file", "<cmd> Telescope find_files <cr>"),
-      dashboard.button("l", "󰒲 " .. " Lazy", "<cmd> Lazy <cr>"),
+      dashboard.button("f", "  Find file", "<cmd> Telescope find_files <cr>"),
+      dashboard.button("l", "󰒲  Lazy", "<cmd> Lazy <cr>"),
       dashboard.button("u", "󱐥  Update plugins", "<cmd>Lazy sync<CR>"),
       dashboard.button("t", "  Install language tools", "<cmd>Mason<CR>"),
-      dashboard.button("s", "" .. " Sessions", "<cmd> Telescope persisted<cr>"),
-      dashboard.button("q", " " .. " Quit", "<cmd> qa <cr>"),
+      dashboard.button("s", "  Sessions", "<cmd> Telescope persisted<cr>"),
+      dashboard.button("q", "  Quit", "<cmd> qa <cr>"),
     }
-		for _, button in ipairs(dashboard.section.buttons.val) do
-			button.opts.hl = "AlphaButtons"
-			button.opts.hl_shortcut = "AlphaShortcut"
-		end
-		dashboard.section.header.opts.hl = "AlphaHeader"
-		dashboard.section.buttons.opts.hl = "AlphaButtons"
-		dashboard.section.footer.opts.hl = "AlphaFooter"
-		dashboard.opts.layout[1].val = 8
-		return dashboard
-	end,
-	config = function(_, dashboard)
-		require("alpha").setup(dashboard.opts)
-	end,
+
+    for _, button in ipairs(dashboard.section.buttons.val) do
+      button.opts.hl = "AlphaButtons"
+      button.opts.hl_shortcut = "AlphaShortcut"
+    end
+
+    dashboard.section.buttons.opts.hl = "AlphaButtons"
+    dashboard.section.footer.opts.hl = "AlphaFooter"
+    dashboard.opts.layout[1].val = 8
+
+    return dashboard
+  end,
+  config = function(_, dashboard)
+    require("alpha").setup(dashboard.opts)
+  end,
 }
